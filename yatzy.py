fc0d6ca332f36c4adadbad7dec9f11e739113d64
jsamosfbmoll <jsamos@cifpfbmoll.eu>
2019-12-05 19:00:59 +0100

    refactor: Refactorizar método yatzy
    Refactorizar método yatzy reescribiendolo por completo, compara el
    primer valor de la lista con los otros para comprobar que todos los
    elementos sean iguales

--- yatzy.py
+++ yatzy.py
@@ -9,13 +9,12 @@ class Yatzy:
 
     @staticmethod
     def yatzy(dice):
-        counts = [0]*(len(dice)+1)
-        for die in dice:
-            counts[die-1] += 1
-        for i in range(len(counts)):
-            if counts[i] == 5:
-                return 50
-        return 0
+        numero = dice[0]
+        for valor in dice:
+            if valor != numero:
+                return 0
+        return 50
+
     
     @staticmethod
     def ones( d1,  d2,  d3,  d4,  d5):
