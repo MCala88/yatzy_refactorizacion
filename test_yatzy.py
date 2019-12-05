8b1846fe54656d974db8fb6484e79f5461ba8bc6
jsamosfbmoll <jsamos@cifpfbmoll.eu>
2019-12-05 18:26:24 +0100

    test: Añadir más casos test
    Añadir más casos test del repositorio original

--- test_yatzy.py
+++ test_yatzy.py
@@ -25,3 +25,28 @@ def test_fours(inyector):
     # No puedo testear con fixtures = inyeccion de dependencias
     # los metodos estaticos como chance()
     assert valorEsperado == inyector.fours()
+
+
+def test_yatzy_uno():
+    assert 50 == Yatzy.yatzy([4, 4, 4, 4, 4])
+    assert 50 == Yatzy.yatzy([2, 2, 2, 2, 2])
+
+
+def test_yatzy_dos():
+    assert 0 == Yatzy.yatzy([1, 5, 6, 3, 2])
+
+
+def test_ones():
+    assert 2 == Yatzy.ones(1, 2, 3, 1, 4)
+
+
+def test_twos():
+    assert 4 == Yatzy.twos(1, 2, 2, 4, 3)
+
+
+def test_pair_uno():
+    assert 12 == Yatzy.score_pair(1, 1, 6, 2, 6)
+
+
+def test_pair_dos():
+    assert 8 == Yatzy.score_pair(3, 3, 3, 4, 4)
